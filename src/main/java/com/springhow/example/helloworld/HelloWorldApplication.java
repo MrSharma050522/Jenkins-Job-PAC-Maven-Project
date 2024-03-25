32c6ddd718464a7742691a074b093531604eaaa7
Sandeep Sharma <110931145+MrSharma050522@users.noreply.github.com>
2024-03-26 00:52:17 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "This is a Test Java Project By Gourav Sharma YT";
     }
 
 }
